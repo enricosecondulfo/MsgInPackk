6c60847fc2139308410a091d10fd8f6c95204a21
Enrico Secondulfo <enrico.secondulfo@fidesdigital.com>
2018-10-08 12:08:14 +0200

    Initial Commit

---
+++ MsgInPackk/MsgInPackk.h
@@ -0,0 +1,19 @@
+//
+//  MsgInPackk.h
+//  MsgInPackk
+//
+//  Created by Enrico Secondulfo on 08/10/2018.
+//  Copyright © 2018 Enrico Secondulfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MsgInPackk.
+FOUNDATION_EXPORT double MsgInPackkVersionNumber;
+
+//! Project version string for MsgInPackk.
+FOUNDATION_EXPORT const unsigned char MsgInPackkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MsgInPackk/PublicHeader.h>
+
+
